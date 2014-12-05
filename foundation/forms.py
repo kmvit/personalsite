65388ea661169157899846c1d73a42312e437305
Андрей <kmv-it@yandex.ru>
2014-12-05 21:02:28 +0300

    Add phone field in contact form

--- foundation/forms.py
+++ foundation/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
+    phone = forms.IntegerField(required=False)
     email = forms.EmailField(max_length=100)
     message = forms.CharField(widget=forms.Textarea(), max_length=1000)
     
